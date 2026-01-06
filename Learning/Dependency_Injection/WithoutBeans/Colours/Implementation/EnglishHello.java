4dcb7d391c168a22c118a0764dfce3cb8f94760d
Paavan Swaroop <paavanswaroop2@gmail.com>
2026-01-06 12:47:33 +0530

    Add EnglishHello class implementing Hello interface

---
+++ Learning/Dependency_Injection/WithoutBeans/Colours/Implementation/EnglishHello.java
@@ -0,0 +1,10 @@
+package com.pavswap.LearningSpringBoot.colours.implementation;
+
+import com.pavswap.LearningSpringBoot.colours.Hello;
+
+public class EnglishHello implements Hello {
+    @Override
+    public String print() {
+        return "Hello";
+    }
+}
