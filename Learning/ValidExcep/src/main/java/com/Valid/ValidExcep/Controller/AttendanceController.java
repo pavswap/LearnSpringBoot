9c82b135debe44e29c904f749140ba3354b7345b
Paavan Swaroop <paavanswaroop2@gmail.com>
2026-05-25 23:58:20 +0530

    Added Logging for Tracking and Debugging

--- Learning/ValidExcep/src/main/java/com/Valid/ValidExcep/Controller/AttendanceController.java
+++ Learning/ValidExcep/src/main/java/com/Valid/ValidExcep/Controller/AttendanceController.java
@@ -1,9 +1,13 @@
 package com.Valid.ValidExcep.Controller;
 
 import com.Valid.ValidExcep.DAO.AttendanceDAO;
-import com.Valid.ValidExcep.Repository.AttendanceRepository;
 import com.Valid.ValidExcep.Service.AttendanceService;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
 import org.springframework.web.bind.annotation.*;
+
 
 import java.util.LinkedList;
 
@@ -13,27 +17,37 @@ public class AttendanceController {
 
     private final AttendanceService attendanceService;
 
+    private final Logger logger = LoggerFactory.getLogger(AttendanceController.class);
+
     public AttendanceController(AttendanceService attendanceService) {
         this.attendanceService = attendanceService;
     }
 
     @GetMapping("/log")
     public LinkedList<AttendanceDAO> log() {
+
+        logger.info("Granted Attendance Log Request");
         return attendanceService.log();
     }
 
     @PostMapping
     public void add(@RequestBody AttendanceDAO dao) {
+
+        logger.info("Added Student {}", dao.getName());
         attendanceService.add(dao);
     }
 
     @DeleteMapping("/{roll}")
     public void remove(@PathVariable int roll) {
+
+        logger.info("Removed Student with roll {}", roll);
         attendanceService.remove(roll);
     }
 
     @GetMapping("/student/{name}")
     public int getRoll(@PathVariable String name) {
+
+        logger.info("Granted Roll for Student {}", name);
         return attendanceService.getRoll(name);
     }
 }
--- Learning/ValidExcep/src/main/java/com/Valid/ValidExcep/ValidExcepApplication.java
+++ Learning/ValidExcep/src/main/java/com/Valid/ValidExcep/ValidExcepApplication.java
@@ -3,6 +3,9 @@ package com.Valid.ValidExcep;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 @SpringBootApplication
 public class ValidExcepApplication {
 
